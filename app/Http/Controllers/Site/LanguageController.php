37d3258f1beb122d825165736f790fe08bde880a
Marisa Endruveit <mendruveit@gmail.com>
2018-01-15 00:42:59 -0200

    Language modificado e resolvido

--- app/Http/Controllers/Site/LanguageController.php
+++ app/Http/Controllers/Site/LanguageController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
 use App;
 use Lang;
 
 class LanguageController extends Controller
 {
-    public function changeLanguage(Request $request){
-        if ($request->ajax()){
-            $request->session()->put('locale', $request->locale);
-            $request->sessions()->flash('alert-success', ('app.Locale_Change_Success'));
-        }
+    public function chooser(){
+       // echo Input::get('locale');
+        Session::put('locale' , Input::get('locale'));
+        return Redirect::back();
     }
     
 }
