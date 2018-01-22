5f1a8c20a5d97e771c96aacbcba86dc14ce851c6
Marisa Endruveit <mendruveit@gmail.com>
2018-01-22 13:59:13 -0200

    Registrar  o usuários antes de logado já com o papel de usuario

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -33,13 +34,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(GateContract $gate)
     {
         $this->registerPolicies($gate);
-
-        foreach ($this->listPermissions() as $permission) {
-            Gate::define($permission->name,function($user) use($permission){
-              return $user->existThisRole($permission->roles) || $user->eAdmin();
-            });
-          }
-
+        if(!app()->runningInConsole()){
+            foreach ($this->listPermissions() as $permission) {
+                Gate::define($permission->name,function($user) use($permission){
+                return $user->existThisRole($permission->roles) || $user->eAdmin();
+                });
+            }
+        }
 
 
         /*
